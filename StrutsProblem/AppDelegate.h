0ed81fa7764bbc6b8e01f944670a917ba30386fc
Jesus Magana <maganazero5@gmail.com>
2014-06-27 08:57:38 -0700

    Initial Commit

---
+++ StrutsProblem/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StrutsProblem
+//
+//  Created by Jesus Magana on 6/27/14.
+//  Copyright (c) 2014 ZeroLinux5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
